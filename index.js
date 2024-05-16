2d7bcf4865978b85736948dd950d84e6189a5219
john.chanthy <john.chanthy@gmail.com>
2024-05-16 16:18:38 +0000

    updade code..

--- index.js
+++ index.js
@@ -4,5 +4,10 @@ async function getFile(fileName) {
     let data = fs.promises.readFile(fileName);
     return (await data).toString();
 }
-let dataGet = await getFile('async.txt');
-console.log(dataGet);
+let dataGet = fs.readFileSync('async.txt');
+// dataGet = getFile('async.txt');
+console.log('code here');
+console.log('more here');
+console.log(await dataGet.toString());
+
+
