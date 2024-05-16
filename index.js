c1a7e762ea54d57003e433b66e6ab48a0b38c432
john.chanthy <john.chanthy@gmail.com>
2024-05-16 15:03:01 +0000

    remove space

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 import fs from 'fs';
 
-fs.readFile('async1.txt', function (err, data) {
+fs.readFile('async.txt', function (err, data) {
     try {
         if (err) throw err;
-        console.log(data.toString())
+        console.log(data.toString());
     } catch (err) {
         console.log('Error');
     }
 });
-
 console.log('done!');
