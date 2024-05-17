c2483c97239cf3c382ab10fa8ee884cff70a6869
U-ser <john.chanthy@gmail.com>
2024-05-18 05:31:28 +0700

    create read stream.

--- index.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-
-async function getFile(fileName) {
-    let data = fs.promises.readFile(fileName);
-    return (await data).toString();
-}
-let dataGet = await getFile('async.txt');
-console.log(dataGet);
