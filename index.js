99a76023aea7df20655aa0bdab549bef6365b55f
john.chanthy <john.chanthy@gmail.com>
2024-05-18 02:43:25 +0000

    using promise

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 import fs from 'fs';
-import * as stringify from 'csv-stringify';
 
-const data = [
-    ['John Doe', '30', 'Software Engineer'],
-    ['Jane Doe', '25', 'Doctor'],
-    ['Alice Smith', '40', 'Teacher']
-];
-const columns = ['Name', 'Age', 'Occupation'];
-const write = fs.createWriteStream('log2.csv');
-
-const stringifier = stringify.stringify({ header: true, columns: columns });
-for (let i = 0; i < data.length; i++) {
-    stringifier.write(data[i]);
+const getFile = (fileName) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(fileName, (err, data) => {
+            err ? reject(err) : resolve(data);
+        });
+    });
 }
-stringifier.pipe(write);
+
+getFile('async.txt')
+    .then(data => console.log(data.toString()))
+    .catch(err => console.log(err));
