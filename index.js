786237a4efeec61996a6d487667fbc92a1166094
john.chanthy <john.chanthy@gmail.com>
2024-05-18 03:15:56 +0000

    using asyn await

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
+import { getFips } from 'crypto';
 import fs from 'fs';
+import { get } from 'http';
 
-const getFile = async (fileName) => {
-    try {
-        const data = await fs.readFile(fileName);
-        return data;
-    } catch (err) {
-        console.log(err);
-    }
-
+async function getfile(fileName) {
+    const data = fs.promises.readFile(fileName);
+    return (await data).toString();
 }
-getFile('async.txt');
+
+const dataGet = await getfile('async.txt');
+console.log(dataGet);
