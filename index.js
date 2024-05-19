cd02d6b1a89f0bf71d43a74732f50058d41a151b
john.chanthy <john.chanthy@gmail.com>
2024-05-19 23:21:04 +0000

    init image

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-const apiUrl = 'https://official-joke-api.appspot.com/jokes/random';
-const getData = async (url) => {
-    return new Promise((resolve, reject) => {
-        fetch(url).then(res => res.json())
-            .then(data => resolve(data))
-            .catch(err => reject(err));
-    });
-}
-const data = await getData(apiUrl);
-console.log(data.type);
-console.log(data.setup);
-console.log(data.punchline);
+const url='https://picsum.photos/200/300';
+const img = async (url) => {
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(img)
+)};
+
+img(url).then(res=> console.log(res));
