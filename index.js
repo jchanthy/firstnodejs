d556f2e42603f6ca1a80befb526e585367f4d0e8
john.chanthy <john.chanthy@gmail.com>
2024-05-21 12:30:43 +0000

    stream read write stream

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const url='https://picsum.photos/200/300';
-const img = async (url) => {
-    fetch(url)
-    .then(res => res.json())
-    .then(data => console.log(img)
-)};
+import fs from 'fs';
+import { Transform } from 'stream';
 
-img(url).then(res=> console.log(res));
+const stream = fs.createReadStream('input.txt');
+
+stream.on('data', function (data) {
+    const chunk = data.toString();
+    console.log(chunk);
+});
+
+stream.on('end', function () {
+    console.log('End of file');
+});
+
+const write_stream = fs.createWriteStream('output.txt');
+
+const trans = new Transform({
+    transform(chunk, _, callback) {
+        callback(null, chunk.toString().toUpperCase());
+    }
+});
+stream.pipe(trans).pipe(write_stream);
