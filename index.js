9df292964f815fd7ac5be03c4c816a99be78eed9
john.chanthy <john.chanthy@gmail.com>
2024-05-23 14:46:50 +0000

    update progess

--- index.js
+++ index.js
@@ -2,11 +2,19 @@
 import { EventEmitter } from 'events';
 const myEventEmitter = new EventEmitter;
 
-function notifyConnection(ip, source) {
-    console.log(ip + ' connected' + ' from ' + source);
+function notifyConnection(ip) {
+    console.log(ip + ' connected');
+}
+
+async function process(ip) {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    console.log('Processing done for ' + ip);
 }
 
 myEventEmitter.on('con', notifyConnection);
+myEventEmitter.on('con', process);
 
 myEventEmitter.emit('con', '127.0.0.1', 'localhost');
+myEventEmitter.emit('con', '127.0.0.1', 'localhost');
+
 
