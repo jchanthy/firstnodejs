a7b1fef938e5f17bc31748fb32fffd886e65dd5b
U-ser <john.chanthy@gmail.com>
2024-05-28 18:53:11 +0700

    create request server.

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 import http from 'http';
-http.createServer((req, res) => {
-    console.log(req, res);
-    res.write("hello world");
-    res.end();
-}).listen(81);
+// http.createServer((req, res) => {
+//     console.log(req, res);
+//     res.write("hello world");
+//     res.end();
+// }).listen(3000);
+
+
+http.createServer((_, res) => {
+   res.writeHead(200, {'Content-Type': 'text/html'});
+   res.write('Hello World!');
+   res.end();
+}).listen(3000);
