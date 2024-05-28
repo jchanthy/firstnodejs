01e7e201f12a893f7769ed80ffb779944dee2dbf
U-ser <john.chanthy@gmail.com>
2024-05-28 19:45:12 +0700

    create request with postman request

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 import http from 'http';
-import url from 'url';
 
 http.createServer((req, res) => {
-    const q = url.parse(req.url, true);
-    console.log(q.query);
-    // const url = url.parse(req.url, true);
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('hello');
+   if(req.method === 'GET') {
+    res.write('Get Request Received');
+   } else {
+       res.write('Other request received');
+   }
     res.end();
 }).listen(3000);
 console.log('Server running at http://localhost:3000/');
