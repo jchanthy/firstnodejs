dc2a23304d2268ed7c167b8e85faf72b98c67ae9
U-ser <john.chanthy@gmail.com>
2024-05-28 20:46:52 +0700

    https service

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
-import http from 'http';
+import https from 'https';
+import * as fs from "node:fs";
 
-http.createServer((req, res) => {
+const options = {
+    key: fs.readFileSync('./keys/private.pem'),
+    cert: fs.readFileSync('./keys/certificate.pem'),
+};
+https.createServer(options, (req, res) => {
    if(req.method === 'GET') {
-    res.write('Get Request Received');
+    res.write('Get request received');
    } else {
-       res.write('Other request received');
+       let body = '';
+       res.on('data', (data) => {
+           body += data;
+           console.log(data);
+       });
+       res.on('end', () => {
+
+           body = JSON.parse(body);
+           console.log(body);
+       });
+       req.on('error', (err) => {
+           console.error(err);
+       })
    }
     res.end();
 }).listen(3000);
